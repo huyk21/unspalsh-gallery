e7155ee345b4d50f420c2abc36c916c9b17f9fc6
Dinh Huy <ldhuy21@clc.fitus.edu.vn>
2024-10-28 13:21:47 +0700

    add cors origin

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api/photos': {
+        target: 'https://api.unsplash.com', // The target API
+        changeOrigin: true, // Bypass CORS by setting the origin
+        rewrite: (path) => path.replace(/^\/api\/photos/, '/photos'),
+      },
+    },
+  },
 })
